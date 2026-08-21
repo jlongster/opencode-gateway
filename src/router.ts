9af08cffe89a56d687ddd7b1f408dc53c101e121
James Long <longster@gmail.com>
2026-08-21 23:21:35 +0000

    revert: remove home form workaround

--- src/router.ts
+++ src/router.ts
@@ -8,7 +8,6 @@ export type Decision =
   | { readonly type: "default-location" }
   | { readonly type: "images" }
   | { readonly type: "image"; readonly name: string }
-  | { readonly type: "empty-global-forms" }
   | { readonly type: "sessions" }
   | { readonly type: "active-sessions" }
   | { readonly type: "events" }
@@ -82,12 +81,6 @@ export function classify(request: Request): Decision {
 
   if (/^\/api\/session\/global\/form(?:\/|$)/.test(url.pathname)) {
     const workspaceID = workspace(request, url);
-    if (
-      !workspaceID &&
-      method === "GET" &&
-      url.pathname === "/api/session/global/form"
-    )
-      return { type: "empty-global-forms" };
     return workspaceID
       ? { type: "workspace", workspaceID }
       : {
